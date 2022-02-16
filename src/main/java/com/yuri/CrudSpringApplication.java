d929ba0aa9908642569196e3164d7e875c4df480
YuriAugusto <komesu.yuri.a@gmail.com>
2022-02-16 10:51:01 -0300

    create HelloController and adding comments in pom.xml

--- src/main/java/com/yuri/CrudSpringApplication.java
+++ src/main/java/com/yuri/CrudSpringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CrudSpringApplication {
 
+	//método responsável por fazer o boot (start) da aplicação java com Spring
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
